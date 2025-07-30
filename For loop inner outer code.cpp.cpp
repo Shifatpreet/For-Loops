7c12f302508662642767d48290c11f1edde42da8
Shifatpreet <shifatpreetsingh@gmail.com>
2025-07-30 23:08:29 +0530

    Add files via upload

---
+++ For loop inner outer code.cpp.cpp
@@ -0,0 +1,32 @@
+//SHIFATPREET SINGH 
+//24070123098
+//Entc B1
+#include <iostream>
+using namespace std;
+int main (){
+    for(int i=1;i<=2;i++){
+    cout<<"Outer:"<<i<<"\n"<<endl;
+
+        for(int j=1;j<=3;j++){
+    cout<<"Inner:"<<j<<"\n"<<endl;
+        }
+    }
+}
+/*
+OUTPUT:-
+
+Outer:1
+
+Inner:1
+
+Inner:2
+
+Inner:3
+
+Outer:2
+
+Inner:1
+
+Inner:2
+
+Inner:3*/
